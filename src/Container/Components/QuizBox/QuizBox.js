c1b700322d473a3cf0c83a2bac0f2ea72684ef89
teneons <teneons@gmail.com>
2020-08-19 19:16:10 +0300

    out quiz-s

--- src/Container/Components/QuizBox/QuizBox.js
+++ src/Container/Components/QuizBox/QuizBox.js
@@ -1,22 +1,43 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 import './QuizBox.scss'
 
-const QuizNow = props => {
+// const QuizNow = props => {
 
 
-    return(<div className={'QuizBox'}>
+//     return(<div className={'QuizBox'}>
+//         <div className={'HeaderTxtQuiz'}>
+//             <span>Whats app?</span>
+//             <span>2/10</span>
+//         </div>
+//         <ul className={'QuizAnswers'}>
+//             {props.quiz.map((answer, index)=>{
+//                 return(
+//                     <li key={index}>{answer}</li>
+//                 )
+//             })}
+//         </ul>
+//     </div>
+//     )
+// }
+
+// export default QuizNow
+
+export default class QuizBox extends Component {
+    render() {
+        return(
+        <div className={'QuizBox'}>
         <div className={'HeaderTxtQuiz'}>
-            <span>Whats app?</span>
-            <span>2/10</span>
+            <span>{this.props.txtHeader}</span>
+            <span>{this.props.key} /5</span>
         </div>
         <ul className={'QuizAnswers'}>
-            <li>A</li>
-            <li>B</li>
-            <li>C</li>
+            {this.props.txtAnswer.map((item)=>{
+                return(
+                    <li>{item}</li>
+                )
+            })}
         </ul>
     </div>
-    )
+    )}
 }
-
-export default QuizNow
--- src/Container/Quiz/Quiz.js
+++ src/Container/Quiz/Quiz.js
@@ -5,14 +5,22 @@ import QuizBox from '../Components/QuizBox/QuizBox'
 
 export default class Quiz extends Component {
     state = {
-        quiz: []
+        quiz: [{txtHeader: 'Quiz 1 ?', txtAnswer: ['A1', 'A2', 'A3'], rightAsnwer: 1},
+                {txtHeader: 'Quiz 2 ?', txtAnswer: ['B1', 'B2', 'B3'], rightAsnwer: 2},
+                {txtHeader: 'Quiz 3 ?', txtAnswer: ['C1', 'C2', 'C3'], rightAsnwer: 2},
+                {txtHeader: 'Quiz 4 ?', txtAnswer: ['D1', 'D2', 'D3'], rightAsnwer: 0},
+                {txtHeader: 'Quiz 5 ?', txtAnswer: ['E1', 'E2', 'E3'], rightAsnwer: 1}]
     }
 
     render() {
         return(
             <div className={'Quiz'}>
                 <span className={'TxtHeader'}>Quiz</span>
-                <QuizBox />
+                {this.state.quiz.map((item, index)=>{
+                    return(
+                        <QuizBox key={index} txtHeader={item.txtHeader} txtAnswer={item.txtAnswer} rightAsnwer={item.rightAsnwer} />
+                    )
+                })}
             </div>
         )
     }
