3abe6ea362d7eca372a3e2c6e276bb4b43dc492c
teneons <teneons@gmail.com>
2020-08-19 12:52:41 +0300

    new structure;connected scss

--- src/Container/App/App.js
+++ src/Container/App/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Layout from '../Layout/Layout'
+
 function App() {
+
   return (
-    <div className="App">
-      
-    </div>
+      <Layout>
+        
+      </Layout>
   );
 }
 
