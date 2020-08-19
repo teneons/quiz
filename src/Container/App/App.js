e2e0622a8152619fc9aa0fdf0f898e9d0a2de4bc
teneons <teneons@gmail.com>
2020-08-19 13:06:10 +0300

    creacte Quiz

--- src/Container/App/App.js
+++ src/Container/App/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import Layout from '../Layout/Layout'
+import Quiz from '../Quiz/Quiz'
 
 function App() {
 
   return (
       <Layout>
-        
+        <Quiz />
       </Layout>
   );
 }
---
+++ src/Container/Quiz/Quiz.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+import './Quiz.scss'
+
+export default class Quiz extends Component {
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
