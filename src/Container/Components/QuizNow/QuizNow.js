ef9e05f7371319462e596c0dd35961635afac6dc
teneons <teneons@gmail.com>
2020-08-19 15:53:24 +0300

    creacte QuizNow; stylization 2 other el.

---
+++ src/Container/Components/QuizNow/QuizNow.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import './QuizNow.scss'
+
+const QuizNow = props => {
+
+
+    return(<div className={'QuizNow'}>
+        <div className={'HeaderTxtQuiz'}>
+            <span>Whats app?</span>
+            <span>2/10</span>
+        </div>
+        <ul>
+            <li>A</li>
+            <li>B</li>
+            <li>C</li>
+        </ul>
+    </div>
+    )
+}
+
+export default QuizNow
--- src/Container/Quiz/Quiz.js
+++ src/Container/Quiz/Quiz.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react'
 
 import './Quiz.scss'
+import QuizNow from '../Components/QuizNow/QuizNow'
 
 export default class Quiz extends Component {
+    state = {
+        quiz: []
+    }
+
     render() {
         return(
             <div className={'Quiz'}>
-                Quiz #42
+                <span className={'TxtHeader'}>Quiz App</span>
+                <QuizNow />
             </div>
         )
     }
